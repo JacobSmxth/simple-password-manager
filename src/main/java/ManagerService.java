b48aee259a6d4ccb8f993beceb0d71880d48cc9b
Jacob Smith <jacob.d.smith@live.com>
2025-10-27 23:44:57 -0400

    Make basic methods to addAccounts, deleteAccounts, showAccounts

--- src/main/java/ManagerService.java
+++ src/main/java/ManagerService.java
@@ -3,4 +3,21 @@ import java.util.List;
 
 public class ManagerService {
     List<Account> accounts = new ArrayList<>();
+
+    public List<Account> getAllAccounts() {
+        return accounts;
+    }
+
+    public Account addAccount(Account account) {
+        accounts.add(account);
+        return account;
+    }
+
+    public void deleteAccount(Account account) {
+        accounts.remove(account);
+    }
+
+    public Account changeAccount(Account account) { // Plan to make an accountDTO thing
+        return account;
+    }
 }
